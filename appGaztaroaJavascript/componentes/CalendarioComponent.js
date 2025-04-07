6c486f1b3daa8b0b95f5e85aeda9a06a14d01811
Rubén Beruete <rubenberuete.1999@gmail.com>
2025-04-07 16:22:56 +0200

    Stack Navigation

--- appGaztaroaJavascript/componentes/CalendarioComponent.js
+++ appGaztaroaJavascript/componentes/CalendarioComponent.js
@@ -1,32 +1,45 @@
-import React from "react";
-import { ListItem, Avatar } from "@rneui/themed";
-import { SafeAreaView, FlatList } from "react-native";
-function Calendario(props) {
-  const renderCalendarioItem = ({ item, index }) => {
+import React, { Component } from 'react';
+import { ListItem, Avatar } from '@rneui/themed';
+import { SafeAreaView, FlatList } from 'react-native';
+import { EXCURSIONES } from '../comun/excursiones';
+
+class Calendario extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            excursiones: EXCURSIONES
+        };
+    }
+
+    render(){
+
+    const { navigate } = this.props.navigation;    
+
+    const renderCalendarioItem = ({item, index}) => {
+        return (
+            <ListItem
+            key={index}
+            onPress={() => navigate('DetalleExcursion', { excursionId: item.id })}
+            bottomDivider>
+                <Avatar source={require('./imagenes/40Años.png')} />
+                <ListItem.Content>
+                    <ListItem.Title>{item.nombre}</ListItem.Title>
+                    <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem> 
+        );
+    };
+
     return (
-      <ListItem 
-      key={index} 
-      onPress={() => props.onPress(item.id)}
-      bottomDivider>
-        
-        <Avatar source={require("./imagenes/40Años.png")} />
-        <ListItem.Content>
-         
-          <ListItem.Title>{item.nombre}</ListItem.Title>
-          <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
+        <SafeAreaView>
+            <FlatList 
+                data={this.state.excursiones}
+                renderItem={renderCalendarioItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </SafeAreaView>
     );
-  };
-  return (
-    <SafeAreaView>
-      
-      <FlatList
-        data={props.excursiones}
-        renderItem={renderCalendarioItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </SafeAreaView>
-  );
+    }
 }
+
 export default Calendario;
